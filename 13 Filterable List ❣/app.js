47b0006f0ba5da9cca199ce48030755015fd63a1
bhuvaneshwaran-kumar <bhuvaneshwarankumar2000@gmail.com>
2021-07-07 11:02:22 +0530

    added template for filterable list😎�

---
+++ 13 Filterable List ❣/app.js
@@ -0,0 +1,26 @@
+const names = [
+    "Annabell Helper",
+    "Laure Cheng",
+    "Bev Dudding",
+    "Clinton Hampshire",
+    "Millard Soderman",
+    "Palmira Hotz",
+    "Fredric Ulloa",
+    "Renetta Steffes",
+    "Martha Bachelder",
+    "Alene Burchard",
+    "Augustus Glatz",
+    "Youlanda Cardona",
+    "Garnett Brignac",
+    "Buffy Vedder",
+    "Mary Storms",
+    "Ervin Salsman",
+    "Marielle Rambin",
+    "Rueben Cothren",
+    "Monnie Whipple",
+    "Jayson Sullivan",
+];
+
+names.sort()
+
+console.log(names)

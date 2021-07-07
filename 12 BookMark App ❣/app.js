e1558d0b840e7d94fed60513df7ac1750dee9d07
bhuvaneshwaran-kumar <bhuvaneshwarankumar2000@gmail.com>
2021-07-07 08:08:32 +0530

    done with core login in bookmark apk😌😌.

--- 12 BookMark App ❣/app.js
+++ 12 BookMark App ❣/app.js
@@ -6,10 +6,10 @@ let bookmarkLocal = [];
 
 //Retrive The bookmark in Local Storage if not present set an empty [] then return.
 function retriveBookmarkLocal() {
-    let arr = localStorage.getItem('bookmarks')
+    let arr = localStorage.getItem('JSMINI_bookmarks')
     arr = JSON.parse(arr)
     if (arr === null) {
-        localStorage.setItem('bookmarks', JSON.stringify([]))
+        localStorage.setItem('JSMINI_bookmarks', JSON.stringify([]))
         arr = []
     }
     return arr
@@ -26,16 +26,6 @@ function validateURL(str) {
     return pattern.test(str);
 }
 
-// console.log(validURL('bhuvaneshwaran-kumar.github.io/JS_MINI_PROJECTS/11%20Image_Carousel%20%E2%9D%A3/index.html'))
-
-const addBookmarkLocal = (name, URL) => {
-    if (!bookmarkLocal) {
-        bookmarkLocal = [{ name: name, URL: URL }]
-        console.log(bookmarkLocal)
-        localStorage.setItem('bookmarks', bookmarkLocal)
-    }
-}
-
 
 // CB-function when user click bookmark_Iteam div.
 function performOnBookmarkItem(e) {
@@ -45,11 +35,13 @@ function performOnBookmarkItem(e) {
         const removeParentElm = targetElm.parentNode
         removeParentElm.classList.add('remove')
         console.log(removeParentElm)
+        const removeIndex = [...bookmarkCollection.children].indexOf(removeParentElm)
         setTimeout(() => {
+            removeBookmarkInLocalStorage(removeIndex)
             bookmarkCollection.removeChild(removeParentElm)
         }, 500)
     } else if (targetElm.classList.contains('bookmark-name')) {
-        window.open('https://www.google.com', 'blank');
+        window.open(targetElm.getAttribute('data-url'));
     }
 }
 
@@ -85,8 +77,53 @@ function formSubmit(e) {
         return bookmarkForm.formUrl.value = ""
     }
 
+    addBookmarkToLocalStorage(formName, formUrl)
+
+    appendUrlElm(formName, formUrl)
+
 }
 
+function createElemet(element, classItems = []) {
+    const Element = document.createElement(element)
+
+    classItems.forEach((elm) => {
+        Element.classList.add(elm)
+    })
+
+    return Element
+}
+
+function appendUrlElm(formName, formUrl) {
+    const bookmarkItemElm = createElemet('div', ['bookmark-item'])
+    const bookmarkNameElm = createElemet('p', ['bookmark-name'])
+    const removeBookmarkElm = createElemet('p', ['remove-bookmark-item'])
+
+
+    bookmarkNameElm.textContent = formName
+    bookmarkNameElm.setAttribute('data-url', formUrl)
+    removeBookmarkElm.textContent = '❌'
+
+    bookmarkItemElm.appendChild(bookmarkNameElm)
+    bookmarkItemElm.appendChild(removeBookmarkElm)
+
+    bookmarkCollection.prepend(bookmarkItemElm)
+
+
+}
+
+function addBookmarkToLocalStorage(formName, formUrl) {
+    bookmarkLocal = [...bookmarkLocal, { name: formName, url: formUrl }]
+    localStorage.setItem('JSMINI_bookmarks', JSON.stringify(bookmarkLocal))
+}
+
+function removeBookmarkInLocalStorage(index) {
+
+    bookmarkLocal = bookmarkLocal.filter((data, dataIndex) => {
+        if (index !== dataIndex) return data
+    })
+
+    localStorage.setItem('JSMINI_bookmarks', JSON.stringify(bookmarkLocal))
+}
 
 
 
@@ -101,4 +138,9 @@ bookmarkForm.addEventListener('submit', formSubmit)
 
 
 bookmarkLocal = retriveBookmarkLocal()
+if (bookmarkLocal.length === 0)
+    appendUrlElm('bhuvan', 'https://bhuvan.com')
+else
+    bookmarkLocal.forEach(data => appendUrlElm(data.name, data.url))
+
 
